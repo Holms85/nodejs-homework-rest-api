fbe50be7514f94e03185b5a228967c6a78919885
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-10-23 18:58:19 +0300

    add jwt and token logic

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,25 +1,38 @@
 const { User } = require("../../models");
 const { Unauthorized } = require("http-errors");
-const joiLoginSchema = require("../../models/user");
+// const joiLoginSchema = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { SECRET_KEY } = process.env;
 
 const login = async(req, res, next) => { 
     try {
-    const { error } = joiLoginSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      error.message = "missing required name field";
-      throw error;
-    }
+    // const { error } = joiLoginSchema.validate(req.body);
+    // if (error) {
+    //   error.status = 400;
+    //   error.message = "missing required name field";
+    //   throw error;
+    // }
         const { email, password } = req.body;
         const user = await User.findOne({ email });
         const passwordCompare = bcrypt.compareSync(password, user.password);
         if (!user || !passwordCompare) {
             throw new Unauthorized("Email or password is wrong");
         }
-        const token = "1234rtyu.d324e8fk.dhjej33sdfrr3a";
+        const payload = {
+            id: user._id
+        }
+        const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "5h" });
         res.json({
-            token
+            status: "success",
+            code: 200,
+            token: token,
+            user: {
+                email: user.email,
+                subscription: user.subscription 
+            }
         })
 } catch (error) {
     next(error)
